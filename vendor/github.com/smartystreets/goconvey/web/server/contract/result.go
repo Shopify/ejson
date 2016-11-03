af796c50f040a2e342c1c6faaac0a25b2ca11938
lxfontes <lxfontes@gmail.com>
2016-11-02 20:59:34 -0400

    vendored dependencies

---
+++ vendor/github.com/smartystreets/goconvey/web/server/contract/result.go
@@ -0,0 +1,107 @@
+package contract
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/smartystreets/goconvey/convey/reporting"
+	"github.com/smartystreets/goconvey/web/server/messaging"
+)
+
+type Package struct {
+	Path          string
+	Name          string
+	Ignored       bool
+	Disabled      bool
+	TestArguments []string
+	Error         error
+	Output        string
+	Result        *PackageResult
+}
+
+func NewPackage(folder *messaging.Folder) *Package {
+	self := new(Package)
+	self.Path = folder.Path
+	self.Name = resolvePackageName(self.Path)
+	self.Result = NewPackageResult(self.Name)
+	self.Ignored = folder.Ignored
+	self.Disabled = folder.Disabled
+	self.TestArguments = folder.TestArguments
+	return self
+}
+
+func (self *Package) Active() bool {
+	return !self.Disabled && !self.Ignored
+}
+
+type CompleteOutput struct {
+	Packages []*PackageResult
+	Revision string
+	Paused   bool
+}
+
+var ( // PackageResult.Outcome values:
+	Ignored         = "ignored"
+	Disabled        = "disabled"
+	Passed          = "passed"
+	Failed          = "failed"
+	Panicked        = "panicked"
+	BuildFailure    = "build failure"
+	NoTestFiles     = "no test files"
+	NoTestFunctions = "no test functions"
+	NoGoFiles       = "no go code"
+)
+
+type PackageResult struct {
+	PackageName string
+	Elapsed     float64
+	Coverage    float64
+	Outcome     string
+	BuildOutput string
+	TestResults []TestResult
+}
+
+func NewPackageResult(packageName string) *PackageResult {
+	self := new(PackageResult)
+	self.PackageName = packageName
+	self.TestResults = []TestResult{}
+	self.Coverage = -1
+	return self
+}
+
+type TestResult struct {
+	TestName string
+	Elapsed  float64
+	Passed   bool
+	Skipped  bool
+	File     string
+	Line     int
+	Message  string
+	Error    string
+	Stories  []reporting.ScopeResult
+
+	RawLines []string `json:",omitempty"`
+}
+
+func NewTestResult(testName string) *TestResult {
+	self := new(TestResult)
+	self.Stories = []reporting.ScopeResult{}
+	self.RawLines = []string{}
+	self.TestName = testName
+	return self
+}
+
+func resolvePackageName(path string) string {
+	index := strings.Index(path, endGoPath)
+	if index < 0 {
+		return path
+	}
+	packageBeginning := index + len(endGoPath)
+	name := path[packageBeginning:]
+	return name
+}
+
+const (
+	separator = string(filepath.Separator)
+	endGoPath = separator + "src" + separator
+)
---
+++ vendor/github.com/smartystreets/goconvey/web/server/parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"log"
+
+	"github.com/smartystreets/goconvey/web/server/contract"
+)
+
+type Parser struct {
+	parser func(*contract.PackageResult, string)
+}
+
+func (self *Parser) Parse(packages []*contract.Package) {
+	for _, p := range packages {
+		if p.Active() {
+			self.parser(p.Result, p.Output)
+		} else if p.Ignored {
+			p.Result.Outcome = contract.Ignored
+		} else if p.Disabled {
+			p.Result.Outcome = contract.Disabled
+		}
+		log.Printf("[%s]: %s\n", p.Result.Outcome, p.Name)
+	}
+}
+
+func NewParser(helper func(*contract.PackageResult, string)) *Parser {
+	self := new(Parser)
+	self.parser = helper
+	return self
+}
