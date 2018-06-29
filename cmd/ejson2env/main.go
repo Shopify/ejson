c3c285583ba05e96261130e6331223155f0ee84c
Catherine Jones <31020910+catherinejones@users.noreply.github.com>
2018-06-29 12:12:48 -0400

    switches to same CLI library as ejson, adds man page

--- cmd/ejson2env/main.go
+++ cmd/ejson2env/main.go
@@ -1,77 +1,84 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
 	"os"
+
+	"github.com/urfave/cli"
 )
-
-// showUsage prints the usage message and the defaults to standard error.
-func showUsage(output io.Writer) {
-	flag.CommandLine.SetOutput(output)
-	fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [-keydir path] [-key-from-stdin] file.ejson\n\n", os.Args[0])
-	flag.CommandLine.PrintDefaults()
-}
-
-// failAndShowUsage prints the error message to stderr, followed
-// by the usage information, before exiting with an error code.
-func failAndShowUsage(err error) {
-	fmt.Fprintf(os.Stderr, "%s\n\n", err)
-	showUsage(os.Stderr)
-	os.Exit(1)
-}
 
 // fail prints the error message to stderr, then ends execution.
 func fail(err error) {
-	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	fmt.Fprintf(os.Stderr, "error: %s\n", err)
 	os.Exit(1)
 }
 
-func main() {
-	var filename string
-	var keydir string
-	var fromStdin bool
-	var showHelp bool
-
-	var err error
-
-	flag.BoolVar(&showHelp, "help", false, "Show this usage message")
-	flag.BoolVar(&fromStdin, "key-from-stdin", false, "Read the private key from STDIN")
-	flag.StringVar(&keydir, "keydir", "/opt/ejson/keys", "Directory containing EJSON keys")
-
-	flag.Parse()
-
-	if showHelp {
-		showUsage(os.Stdout)
-		os.Exit(0)
-	}
-
-	if 1 <= flag.NArg() {
-		filename = flag.Arg(0)
-	}
-
-	if "" == filename {
-		failAndShowUsage(fmt.Errorf("no secrets.ejson filename passed"))
-	}
-
-	var userSuppliedPrivateKey string
-	if fromStdin {
-		userSuppliedPrivateKey, err = readKey(os.Stdin)
-		if err != nil {
-			fail(fmt.Errorf("failed to read from stdin: %s", err))
-		}
-	}
-
-	secrets, err := ReadSecrets(filename, keydir, userSuppliedPrivateKey)
+// exportSecrets wraps the read, extract, and export steps. Returns
+// an error if any step fails.
+func exportSecrets(filename, keyDir, privateKey string) error {
+	secrets, err := ReadSecrets(filename, keyDir, privateKey)
 	if nil != err {
-		fail(fmt.Errorf("could not load ejson file: %s", err))
+		return (fmt.Errorf("could not load ejson file: %s", err))
 	}
 
 	envValues, err := ExtractEnv(secrets)
 	if nil != err {
-		fail(fmt.Errorf("could not load environment from file: %s", err))
+		return fmt.Errorf("could not load environment from file: %s", err)
 	}
 
 	ExportEnv(os.Stdout, envValues)
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Usage = "get environment variables from ejson files"
+	app.Version = VERSION
+	app.Author = "Catherine Jones"
+	app.Email = "catherine.jones@shopify.com"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "keydir, k",
+			Value:  "/opt/ejson/keys",
+			Usage:  "Directory containing EJSON keys",
+			EnvVar: "EJSON_KEYDIR",
+		},
+		cli.BoolFlag{
+			Name:  "key-from-stdin",
+			Usage: "Read the private key from STDIN",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		var filename string
+
+		keydir := c.String("keydir")
+
+		var userSuppliedPrivateKey string
+		if c.Bool("key-from-stdin") {
+			var err error
+			userSuppliedPrivateKey, err = readKey(os.Stdin)
+			if err != nil {
+				fail(fmt.Errorf("failed to read from stdin: %s", err))
+			}
+		}
+
+		if 1 <= len(c.Args()) {
+			filename = c.Args().Get(0)
+		}
+
+		if "" == filename {
+			fail(fmt.Errorf("no secrets.ejson filename passed"))
+		}
+
+		if err := exportSecrets(filename, keydir, userSuppliedPrivateKey); nil != err {
+			fail(err)
+		}
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, "Unexpected failure:", err)
+		os.Exit(1)
+	}
+
 }
