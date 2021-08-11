b5a7eeb6e05d7c009a9eb435afd09abc019881a8
ant Kenworthy <mcrmonkey@users.noreply.github.com>
2021-08-11 21:03:11 +0100

    Remove manpage call
    Let the cli module handle help instead. Works better where man isn't present

--- cmd/ejson/main.go
+++ cmd/ejson/main.go
@@ -23,20 +23,6 @@ func main() {
 	// Encryption is expensive. We'd rather burn cycles on many cores than wait.
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	if runtime.GOOS != "windows" {
-		// Rather than using the built-in help printer, display the bundled manpages.
-		cli.HelpPrinter = func(w io.Writer, templ string, data interface{}) {
-
-			if cmd, ok := data.(cli.Command); ok {
-				switch cmd.Name {
-				case "encrypt", "decrypt", "keygen":
-					execManpage("1", "ejson-"+cmd.Name)
-				}
-			}
-			execManpage("1", "ejson")
-		}
-	}
-
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
